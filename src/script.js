d75175b45efb86afa49fcde139e94ed10a753c1b
gatefait-mathilde <mathilde.gatefait@edu.univ-fcomte.fr>
2024-12-13 16:26:18 +0100

    css+ fivicon

--- src/script.js
+++ src/script.js
@@ -37,3 +37,25 @@ document.body.addEventListener("click", (evt)=> {
         lightBox.close();
     }, {once: true});
 });
+
+
+/*carrousel*/ 
+
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+const premierItem = document.querySelector(".carousel__item");
+
+
+const scrollAmount = premierItem ? premierItem.clientWidth : 0;
+
+if (carousel && scrollAmount > 0) {
+  
+  prevButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+  });
+
+  nextButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  });
+}
